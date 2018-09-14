ef86ce4bdf88c2c7608918ab281fc3f434c09059
Yutong Liu <Musketeer.Liu@gmail.com>
2018-09-14 15:21:07 -0700

    Debug: Anti-Uncomment

--- populate.py
+++ populate.py
@@ -1,43 +1,43 @@
-# import os
-# import random
-# import django
-# from faker import Faker
+import os
+import random
+import django
+from faker import Faker
 
 
-# from django.contrib.auth.models import User
-# from blog.models import Post, Comment
+from django.contrib.auth.models import User
+from blog.models import Post, Comment
 
 
 
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-# django.setup()
-# fake = Faker()
-# USERS = ['A', 'B', 'C', 'D', 'E']
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+django.setup()
+fake = Faker()
+USERS = ['A', 'B', 'C', 'D', 'E']
 
 
-# # def add_user():
-# #     u = User.objects.get_or_create(username=random.choice(USERS))[0]
-# #     u.save()
-# #     return u
+# def add_user():
+#     u = User.objects.get_or_create(username=random.choice(USERS))[0]
+#     u.save()
+#     return u
 
 
-# def populate():
-#     for user in USERS:
-#         author = user
+def populate():
+    for user in USERS:
+        author = user
         
-#         fake_title = fake.sentence()
-#         fake_author = fake.name()
-#         fake_text = fake.text()
+        fake_title = fake.sentence()
+        fake_author = fake.name()
+        fake_text = fake.text()
 
-#         p = Post.objects.get_or_create(author = author, title=fake_title, text=fake_text)[0]
-#         c = Comment.objects.get_or_create(post=p, author=fake_author, text=fake_text, approved_comment=True)[0]
-
-
+        p = Post.objects.get_or_create(author = author, title=fake_title, text=fake_text)[0]
+        c = Comment.objects.get_or_create(post=p, author=fake_author, text=fake_text, approved_comment=True)[0]
 
 
-# if __name__ == '__main__':
-#     print('Population Script:')
-#     populate()
-#     print()
+
+
+if __name__ == '__main__':
+    print('Population Script:')
+    populate()
+    print()
 
